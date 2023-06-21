0a98797d95821a9c6f0083dd735ddd69e831278a
Warren Barney <warren.barney1973@gmail.com>
2023-06-20 22:19:03 -0700

    Test hope this works new

---
+++ alien_invasion.py
@@ -0,0 +1,52 @@
+import sys
+
+import pygame
+
+from settings import Settings
+
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+        from ship import Ship
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
+        pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+
+        # Set the background color.
+        self.bg_color = (230, 230, 230)
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            self._check_events()
+            self._update_screen()
+            self.clock.tick(60)
+
+    def _update_screen(self):
+        """updating images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+
+if __name__ == "__main__":
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
