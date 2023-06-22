40701c8f46989d1fe8a3601dad0cc50b6b9214ed
Warren Barney <warren.barney1973@gmail.com>
2023-06-22 15:57:15 -0700

    move right

--- alien_invasion.py
+++ alien_invasion.py
@@ -26,6 +26,7 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             self.clock.tick(60)
 
@@ -34,6 +35,12 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
 
     def _update_screen(self):
         """updating images on the screen, and flip to the new screen."""
