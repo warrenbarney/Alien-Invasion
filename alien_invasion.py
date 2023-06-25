dacb94f11388903ed2a6e9543a8b7c78f9fbdad2
Warren Barney <warren.barney1973@gmail.com>
2023-06-24 19:43:41 -0700

    Fleet moves side by side and down

--- alien_invasion.py
+++ alien_invasion.py
@@ -83,7 +83,8 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
 
     def _update_aliens(self):
-        """Update the positions of all aiens in the fleet."""
+        """Check if fleet is at the edge, then update positions."""
+        self._check_fleet_edges()
         self.aliens.update()
 
     def _create_fleet(self):
@@ -110,6 +111,19 @@ class AlienInvasion:
         new_alien.rect.x = x_position
         new_alien.rect.y = y_position
         self.aliens.add(new_alien)
+
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _update_screen(self):
         """updating images on the screen, and flip to the new screen."""
--- settings.py
+++ settings.py
@@ -7,8 +7,12 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
-        # Alien stiings
+
+        # Alien settings.
         self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
 
         # Ship settings
         self.ship_speed = 1.5
